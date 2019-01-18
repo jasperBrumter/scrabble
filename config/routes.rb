c22c6f0dadc807e268fe837add49f8580055c58e
Jasper <jasper@jasperPro.local>
2019-01-18 01:34:14 +0100

    implemented cheat version

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+	root to: "pages#play"
+  post 'score', to: "pages#score", as: :score
+  post "find", to: "pages#find", as: :find
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
