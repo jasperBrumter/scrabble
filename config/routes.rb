4939e3864dc65da8491d4f47f13789e4c1367f99
Jasper <jasper@jasperPro.local>
2019-01-18 18:31:15 +0100

    improve routing flow

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   post 'score', to: "pages#score", as: :score
   post "find", to: "pages#find", as: :find
   get "restart", to: "pages#restart", as: :restart
+  get "score", to: "pages#play"
+  get "find", to: "pages#play"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
