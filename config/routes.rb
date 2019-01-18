ba7a9f66ad65563769df6af444abb50c00d74931
Jasper <jasper@jasperPro.local>
2019-01-18 18:27:14 +0100

    add score session

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
 	root to: "pages#play"
   post 'score', to: "pages#score", as: :score
   post "find", to: "pages#find", as: :find
+  get "restart", to: "pages#restart", as: :restart
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
