34111e224e102858a129ce1456e56e8f7386a5ff
Jasper <jasper@jasperPro.local>
2019-01-19 15:57:52 +0100

    fix meta tags

--- app/helpers/meta_tags_helper.rb
+++ app/helpers/meta_tags_helper.rb
@@ -2,16 +2,14 @@
 
 module MetaTagsHelper
   def meta_title
-    content_for?(:meta_title) ? content_for(:meta_title) : DEFAULT_META["meta_title"]
+    "Scrabble Helper - Cheat or Train your way to winning!"
   end
 
   def meta_description
-    content_for?(:meta_description) ? content_for(:meta_description) : DEFAULT_META["meta_description"]
+    "The first bilingual (French and English) Scrabble helper, ask us to find the longest words with your letters, or increase your skill by playing our fun game!"
   end
 
   def meta_image
-    meta_image = (content_for?(:meta_image) ? content_for(:meta_image) : DEFAULT_META["meta_image"])
-    # little twist to make it work equally with an asset or a url
-    meta_image.starts_with?("http") ? meta_image : image_url(meta_image)
+    "thumbnail.jpg" 
   end
 end
