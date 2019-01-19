cb5fea81c39698e33997719a88d9d62a70b0faf4
Jasper <jasper@jasperPro.local>
2019-01-19 16:54:02 +0100

    english version

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,12 @@ Rails.application.routes.draw do
   get "restart", to: "pages#restart", as: :restart
   get "score", to: "pages#play"
   get "find", to: "pages#play"
+
+  get "english", to: "english#play", as: :english_play
+  post "english/score", to: "english#score", as: :english_score
+  post "english/find", to: "english#find", as: :english_find
+  get "english/restart", to: "english#restart", as: :english_restart
+  get "english/score", to: "english#play"
+  get "english/find", to: "english#play"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
